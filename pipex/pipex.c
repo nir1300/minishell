04cd15346550b1d34b67c5935822a535a2a8df85
nir1300 <73449726+nir1300@users.noreply.github.com>
2025-02-11 16:33:33 +0000

    trying to incorporate heredoc into pipex

--- pipex/pipex.c
+++ pipex/pipex.c
@@ -14,13 +14,20 @@
 
 void	initialise(int argc, char *argv[], t_pipe *data)
 {
+	int heredoc = 0;
 	data->args = NULL;
-	data->path = NULL;
+	//data->path = NULL;
 	data->path = NULL;
 	data->pipe_fd = NULL;
 	if (argc < 5)
 		free_struct(data, errno);
-	data->input_fd = open(argv[1], O_RDONLY);
+	// if (data->path == NULL)
+	// 	data->input_fd = open(argv[1], O_RDONLY);
+	else
+	{
+		data->input_fd = open("heredoc", O_WRONLY | O_CREAT | O_TRUNC, 0644);
+		heredoc++;
+	}
 	if (data->input_fd < 0)
 		perror(argv[1]);
 	data->output_fd = open(argv[argc - 1], O_WRONLY | O_CREAT | O_TRUNC, 0644);
@@ -30,7 +37,7 @@ void	initialise(int argc, char *argv[], t_pipe *data)
 		close(data->input_fd);
 		free_struct(data, 1);
 	}
-	data->cmdcount = argc - 3;
+	data->cmdcount = argc - 3 - heredoc;
 }
 
 void	create_cmd(t_pipe *data, char *argv[], int i)
@@ -93,28 +100,57 @@ void	middle_child(t_pipe *data, char *argv[], int i, char *envp[])
 	}
 }
 
+void	handle_heredoc(int argc, char *argv[], t_pipe *data)
+{
+    char buffer[1024];
+    ssize_t bytes_read;
+    char *delimitor;
+    (void)argc;
+    delimitor = ft_strdup(argv[2]);
+    while (1)
+    {
+        write(0, "> ", 2);
+        bytes_read = read(0, buffer, sizeof(buffer - 1)); // use gnl instead?
+        if (bytes_read <= 0)
+            break;
+        buffer[bytes_read - 1] = '\0';
+        if (ft_strcmp(buffer, delimitor) == 0)
+            break;
+		buffer[bytes_read - 1] = '\n';
+		buffer[bytes_read] = '\0';
+		write(data->input_fd, &buffer, strlen(buffer));
+    }
+    free (delimitor);
+}
+
 int	main(int argc, char *argv[], char *envp[])
 {
 	t_pipe	*data;
 	int		i;
 
+	i = 0;
 	if (argc < 5)
 		exit(errno);
 	data = malloc(sizeof(t_pipe) + (sizeof(pid_t) * (argc - 3)));
 	if (!data)
 		exit(errno);
 	initialise(argc, argv, data);
+	if (ft_strcmp(argv[1], "here_doc") == 0)
+	{
+		i = 1;
+		handle_heredoc(argc, argv, data);
+	}
+	return (0);
 	data->pipe_fd = malloc(sizeof(int) * (data->cmdcount - 1) * 2);
 	if (!data->pipe_fd)
 		free_struct(data, errno);
-	i = 0;
 	while (i < (data->cmdcount - 1))
 	{
 		if (pipe(&data->pipe_fd[i++ *2]) == -1)
 			free_struct(data, errno);
 	}
 	first_child(data, argv, envp);
-	i = 1;
+	i = 1 + 1;
 	while (i < data->cmdcount)
 		middle_child(data, argv, i++, envp);
 	ft_close(data);
